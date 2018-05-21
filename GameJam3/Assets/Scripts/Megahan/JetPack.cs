cdfcbd69330b1539b8d4f092d667371b86661e57
Meaghan Rattle <residentunleashed@gmail.com>
2018-05-21 13:46:59 +1000

    Jetpack Update
    Included a lerp starting from zero and going towards a target height

--- GameJam3/Assets/Scripts/Megahan/JetPack.cs
+++ GameJam3/Assets/Scripts/Megahan/JetPack.cs
@@ -11,6 +11,9 @@ public class JetPack : MonoBehaviour
     private float timer = 0.0f;
     private bool launch = false;
     private float flightSpeed = 0.0f;
+    private float lerpTime = 1.0f;
+    private float currentLerpTime = 0.0f;
+    private float targetHeight;
     #endregion
 
     #region Getters and Setters
@@ -31,36 +34,42 @@ public class JetPack : MonoBehaviour
         get { return burnTime; }
         set { burnTime = value; }
     }
-
-
     #endregion
 
-
-    // Use this for initialization
-    void Start ()
+    void Update()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if(launch)
+        if (launch)
         {
+            //Increase timer
+            timer += Time.deltaTime;
+
+            //Increment timer once per frame
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
             if (timer < burnTime)
             {
-                //Calulate flightSpeed
-                flightSpeed = vSpeed - weight * Time.deltaTime;
+                //Begin to lerp
+                float t = currentLerpTime / lerpTime;
+                t = 1.0f - Mathf.Cos(t * Mathf.PI * 0.5f);
+
 
                 //Move up
-                transform.position += flightSpeed * Vector3.up;
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(0, targetHeight, t), transform.position.z);
             }
         }
-	}
+    }
 
 
-    void TakeOff()
+    public void TakeOff()
     {
+        //Calculate target height
+        targetHeight = (vSpeed - weight) * burnTime;
+
+        //Launch
         launch = true;
     }
 }
