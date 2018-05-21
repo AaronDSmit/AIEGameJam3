3c8b5633ebe46d70d0fa3b77b785dfd692e87d6a
AIE\s171041 <smitaaron1@gmail.com>
2018-05-22 09:50:06 +1000

    Added functionality for the fuel indicator. Can be updated in the jetpack

---
+++ GameJam3/Assets/Scripts/Aaron/UI/FuelHUD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelHUD : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHeight = 1000.0f;
+
+    private RectTransform fuelIndicator;
+
+    private void Awake()
+    {
+        fuelIndicator = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        UpdateFuel(1.0f);
+    }
+
+    public void UpdateFuel(float percent)
+    {
+        fuelIndicator.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, percent * maxHeight);
+    }
+}
