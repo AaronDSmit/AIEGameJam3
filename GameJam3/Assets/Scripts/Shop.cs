488eca649d007ace98d3fbc460cbb8626dcf3c08
AIE\s171041 <smitaaron1@gmail.com>
2018-05-21 12:20:09 +1000

    Added Shop Script.

---
+++ GameJam3/Assets/Scripts/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField]
+    private Part[] fuelTanks;
+
+    [SerializeField]
+    private Part[] thruster;
+
+    [SerializeField]
+    private Part[] body;
+}
+
+[System.Serializable]
+public struct Part
+{
+    public float weight;
+
+    public float thrustPower;
+
+    public float thrustDuration;
+};
