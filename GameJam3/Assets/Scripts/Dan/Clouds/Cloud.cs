482dc59b7e3126cb92c5ea923e6e8588ef2d84cb
Daniel_Studdert <studdert249@gmail.com>
2018-05-23 00:45:28 +1000

    Cloud Generator basics

---
+++ GameJam3/Assets/Scripts/Dan/Clouds/Cloud.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+	private bool isInitialised;
+	private CloudGenerator cloudGenerator;
+	private Vector3 direction;
+	private float depth;
+	private float speed;
+	private float despawnXPosition;
+
+	public void Init(CloudGenerator cloudGenerator, Vector3 direction, float depth, float speed, float despawnXPosition) {
+		this.cloudGenerator = cloudGenerator;
+		this.direction = direction;
+		this.depth = depth;
+		this.speed = speed;
+		this.despawnXPosition = despawnXPosition;
+
+		isInitialised = true;
+	}
+
+	private void Update() {
+		UpdateMovement();
+	}
+
+	private void UpdateMovement() {
+		if (isInitialised == false)
+			return;
+
+		Vector3 newLocation = transform.position + (direction * speed * Time.deltaTime);
+		newLocation.z = depth;
+
+		transform.position = newLocation;
+
+		if(despawnXPosition < 0 && transform.position.x <= despawnXPosition ||
+			despawnXPosition > 0 && transform.position.x >= despawnXPosition) {
+			cloudGenerator.CloudDespawned();
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ GameJam3/Assets/Scripts/Dan/Clouds/CloudGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour {
+	[SerializeField] private GameObject followObject;
+
+	[Header("Spawn Area")]
+	[SerializeField] private float spawnHeight;
+	[SerializeField] private float despawnHeight;
+	[SerializeField] private float minXPosition;
+	[SerializeField] private float maxXPosition;
+
+	[Header("Spawn Frequencies")]
+	[SerializeField] private float minSpawnTime;
+	[SerializeField] private float maxSpawnTime;
+	[SerializeField] private float minCloudNumber;
+	[SerializeField] private float maxCloudNumber;
+
+	[Header("Cloud Objects")]
+	[SerializeField] private List<GameObject> clouds;
+	[SerializeField] private List<float> depths;
+	[SerializeField] private float minCloudSpeed;
+	[SerializeField] private float maxCloudSpeed;
+
+	private bool producingClouds;
+	private float currentSpawnTime;
+	private float spawnCount;
+
+	private int currentCloudNumber;
+	private Transform cloudParent;
+
+	private void Start() {
+		ResetSpawnTime();
+
+		cloudParent = new GameObject().transform;
+		cloudParent.name = "Cloud Parent";
+	}
+
+	public void ToggleProduction(bool producing) {
+		producingClouds = producing;
+	}
+
+	private void Update() {
+		UpdatePosition();
+		UpdateCloudSpawn();
+	}
+
+	public void CloudDespawned() {
+		currentCloudNumber--;
+	}
+
+	private void UpdatePosition() {
+		if (followObject == null)
+			return;
+
+		transform.position = followObject.transform.position;
+	}
+
+	private void UpdateCloudSpawn() {
+		if (currentCloudNumber >= maxCloudNumber)
+			return;
+
+		if (currentCloudNumber < minCloudSpeed) {
+			SpawnCloud();
+			return;
+		}
+
+		spawnCount += Time.deltaTime;
+
+		if(spawnCount >= currentSpawnTime) {
+			ResetSpawnTime();
+			SpawnCloud();
+		}
+	}
+
+	private void SpawnCloud() {
+		currentCloudNumber++;
+
+		GameObject cloud = Instantiate(clouds[Random.Range(0, clouds.Count)]);
+		cloud.transform.SetParent(cloudParent);
+
+		float xPosition = Random.Range(0, 2) == 0 ? Random.Range(minXPosition, 0) : Random.Range(0, maxXPosition);
+		float zPosition = depths[Random.Range(0, depths.Count)];
+
+		Vector3 position = new Vector3(xPosition,
+			transform.position.y + spawnHeight,
+			zPosition);
+
+		cloud.transform.position = position;
+		cloud.GetComponent<Cloud>().Init(this, xPosition < 0 ? Vector3.right : Vector3.left, zPosition,
+			Random.Range(minCloudSpeed, maxCloudSpeed), 
+			xPosition == minXPosition ? maxXPosition : minXPosition);
+	}
+
+	private void ResetSpawnTime() {
+		spawnCount = 0f;
+		currentSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y + spawnHeight, transform.position.z), 
+			new Vector3(Mathf.Abs(minXPosition) + maxXPosition, 1, 1));
+
+		Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y + despawnHeight, transform.position.z),
+			new Vector3(Mathf.Abs(minXPosition) + maxXPosition, 1, 1));
+	}
+}
