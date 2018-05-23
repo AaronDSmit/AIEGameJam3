f8f04534b9c3f83bb06996a518afb21171785e60
Meaghan Rattle <residentunleashed@gmail.com>
2018-05-23 10:49:17 +1000

    Soundscript
    Added functionality to sounds

--- GameJam3/Assets/Scripts/Meaghan/PlaySound.cs
+++ GameJam3/Assets/Scripts/Meaghan/PlaySound.cs
@@ -13,6 +13,21 @@ public class PlaySound : MonoBehaviour
 
     [SerializeField]
     private AudioClip fireButtonClip;
+
+    [SerializeField]
+    private AudioClip winClip;
+
+    [SerializeField]
+    private AudioClip loseClip;
+
+    [SerializeField]
+    private AudioClip animalNoise1;
+
+    [SerializeField]
+    private AudioClip animalNoise2;
+
+    [SerializeField]
+    private AudioClip animalNoise3;
     #endregion
 
 
@@ -26,7 +41,31 @@ public class PlaySound : MonoBehaviour
         audioSource.PlayOneShot(fireButtonClip, 0.5f);
     }
 
+    public void PlayWinSound()
+    {
+        audioSource.PlayOneShot(winClip, 0.5f);
+    }
 
+    public void PlayLoseSound()
+    {
+        audioSource.PlayOneShot(loseClip, 0.5f);
+    }
 
+    void RandomCreatureNoise()
+    {
+        int select = Random.Range(0, 2);
 
+        if(select == 0)
+        {
+            audioSource.PlayOneShot(animalNoise1, 0.5f);
+        }
+        else if(select == 1)
+        {
+            audioSource.PlayOneShot(animalNoise2, 0.5f);
+        }
+        else if (select == 2)
+        {
+            audioSource.PlayOneShot(animalNoise3, 0.5f);
+        }
+    }
 }
