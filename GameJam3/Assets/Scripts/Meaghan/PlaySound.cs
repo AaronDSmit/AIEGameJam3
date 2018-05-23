c3b69418d3bee538eb3dc8e35ab4a4eea8677e0d
Meaghan Rattle <residentunleashed@gmail.com>
2018-05-23 10:40:52 +1000

    Audiosource added

---
+++ GameJam3/Assets/Scripts/Meaghan/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    #region Inspector Variables
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip buttonClip;
+
+    [SerializeField]
+    private AudioClip fireButtonClip;
+    #endregion
+
+
+    public void PlayButtonSound()
+    {
+        audioSource.PlayOneShot(buttonClip, 0.5f);
+    }
+
+    public void PlayFireButtonSound()
+    {
+        audioSource.PlayOneShot(fireButtonClip, 0.5f);
+    }
+
+
+
+
+}
